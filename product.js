8f973f1d7dc62549c9a6b1c5930cf3d69b94cac3
unknown <vladyslavvasylenko03@gmail.com>
2021-08-18 15:08:47 +0300

    fixse text

--- product.js
+++ product.js
@@ -121,3 +121,4 @@ searchBtn.onclick = function() {
     searchInput.focus();
   }
 }
+
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict"
 
 const answer = [
-  { HTML:"(от англ. HyperText Markup Language — «язык гипертекстовой разметки») — стандартизированный язык разметки документов для просмотра веб-страниц в браузере. Веб-браузеры получают HTML документ от сервера по протоколам HTTP/HTTPS или открывают с локального диска, далее интерпретируют код в интерфейс, который будет отображаться на экране монитора.", },
-  { CSS:"(/siːɛsɛs/ англ. Cascading Style Sheets «каскадные таблицы стилей») — формальный язык описания внешнего вида документа (веб-страницы), написанного с использованием языка разметки (чаще всего HTML или XHTML). Также может применяться к любым XML-документам, например, к SVG или XUL.", },
-  { JS:"Мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript (стандарт ECMA-262.7).", },
+  { HTML:"(HyperText Markup Language) is the most basic building block of the Web. It defines the meaning and structure of web content. Other technologies besides HTML are generally used to describe a web page's appearance/presentation (CSS) or functionality/behavior (JavaScript).", },
+  { CSS:"(/ siːɛsɛs / eng. Cascading style sheets 'cascading style sheets') is a formal language for describing the appearance of a document (web page) written using a markup language (most often HTML or XHTML). It can also be used on any XML document such as SVG or XUL.", },
+  { JS:"is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. ", },
 ];
 
 const btnClick = document.querySelector('.form__button');
@@ -18,7 +18,7 @@ btnClick.onclick = function(e) {
   } else if ( input.value == 'JS' ) {
     document.querySelector('.result').innerHTML = `<p class="text"><b>JS</b> - ${answer[2].JS}</p>`;
   } else {
-    document.querySelector('.result').innerHTML = '<p class="text">Sorry we don`t have information</p>';
+    document.querySelector('.result').innerHTML = '<p class="text">Sorry we don`t have information.</p>';
   }
 }
 
@@ -36,7 +36,9 @@ btnClick2.onclick = function(e) {
   let multiplic = Number(userNumber1.value) * Number(userNumber2.value);
 
   if ( userNumber1.value == "" ) {
-    document.querySelector('#result').innerHTML = '<p class="text"><b>Send number</b></p>';
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
+  } else if ( userNumber2.value == '' ) {
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
   } else {
     result.innerHTML = '<p class="text">' + '<b>' + 'Your result: ' + '<br>' + userNumber1.value + ' <i class="fas fa-times size-icon"></i> ' + userNumber2.value + ' <i class="fas fa-equals size-icon"></i> ' + multiplic + '</b>' + '</p>';
   }
@@ -53,7 +55,9 @@ btnClick3.onclick = function(e) {
   let plus = Number(userNumber3.value) + Number(userNumber4.value);
 
   if ( userNumber3.value == '') {
-    document.querySelector('#result').innerHTML = '<p class="text"><b>Send number</b></p>';
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
+  } else if ( userNumber4.value == '' ) {
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
   } else {
     result.innerHTML = '<p class="text">' + '<b>' + 'Your result: ' + '<br>' + userNumber3.value + ' <i class="fas fa-plus size-icon"></i> ' + userNumber4.value + ' <i class="fas fa-equals size-icon"></i> ' + plus + '</b>' + '</p>';
   }
@@ -70,7 +74,9 @@ btnClick4.onclick = function(e) {
   let minus = Number(userNumber5.value) - Number(userNumber6.value);
 
   if ( userNumber5.value == '') {
-    document.querySelector('#result').innerHTML = '<p class="text"><b>Send number</b></p>';
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
+  } else if ( userNumber6.value == '' ) {
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
   } else {
     result.innerHTML = '<p class="text">' + '<b>' +  'Your result: ' + '<br>' + userNumber5.value + ' <i class="fas fa-minus size-icon"></i> ' + userNumber6.value + ' <i class="fas fa-equals size-icon"></i> ' + minus + '</b>' + '</p>';
   }
@@ -87,7 +93,9 @@ btnClick5.onclick = function(e) {
   let division = Number(userNumber7.value) / Number(userNumber8.value);
 
   if ( userNumber7.value == '' ) {
-    document.querySelector('#result').innerHTML = '<p class="text"><b>Send number</b></p>';
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
+  } else if ( userNumber8.value == '' ) {
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
   } else {
     result.innerHTML = '<p class="text">' + '<b>' + 'Your result: ' + '<br>' + userNumber7.value + ' <i class="fas fa-divide size-icon"></i> ' + userNumber8.value + ' <i class="fas fa-equals size-icon"></i> ' + division + '</b>' + '</p>';
   }
@@ -104,7 +112,9 @@ btnClick6.onclick = function(e) {
   let division = Number(userNumber9.value) / 100 * Number(userNumber10.value);
 
   if ( userNumber9.value == '' ) {
-    document.querySelector('#result').innerHTML = '<p class="text"><b>Send number</b></p>';
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
+  } else if ( userNumber10.value == '' ) {
+    document.querySelector('#result').innerHTML = '<p class="text"><b>Enter the number.</b></p>';
   } else {
     result.innerHTML = '<p class="text">' + '<b>' + 'Your result: ' + '<br>' + userNumber9.value + '<i class="fas fa-percent size-icon"></i> from ' + userNumber10.value + ' <i class="fas fa-equals size-icon"></i> ' + division + '</b>' + '</p>';
   }
