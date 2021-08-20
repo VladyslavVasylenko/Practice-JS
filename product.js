ddeb37aa2950b01a606a1ba0850741212049db30
unknown <vladyslavvasylenko03@gmail.com>
2021-08-20 12:25:36 +0300

    all project

--- product.js
+++ product.js
@@ -122,3 +122,84 @@ searchBtn.onclick = function() {
   }
 }
 
+// addEventListener
+
+// function modifyText() {
+//   let table__item2 = document.getElementById('table__item2');
+//   if (table__item2.firstChild.nodeValue == 'Three') {
+//     table__item2.firstChild.nodeValue = 'Two';
+//   } else {
+//     table__item2.firstChild.nodeValue = 'Three';
+//   }
+// }
+
+// function addClass() {
+//   let addsClass = document.querySelector('.two');
+
+//   if ( modifyText == 'two') {
+//     addsClass.classList.toggle("two"); 
+//   } else {
+//     addsClass.classList.toggle("three"); 
+//   }
+// }
+
+// let el = document.getElementById("table");
+// el.addEventListener('click', modifyText, false);
+
+// let elclass = document.getElementById("table");
+// elclass.addEventListener('click', addClass, false);
+
+// Object.prototype.constructor
+function Tree(name) {
+  this.name = name;
+}
+
+let theTree = new Tree('Red tree');
+console.log('theTree.constructor = ' + theTree);
+
+// Rabbit
+// function Rabbit(name) {
+//   this.name = name;
+//   console.log(name);
+// }
+
+// let rabbit = new Rabbit('White Rabbit');
+
+// let rabbitSecond = new rabbit.constructor('Black Rabbit');
+
+// console.log(rabbit);
+// console.log(rabbitSecond);
+// 
+// function Rabbit() {}
+// Rabbit.prototype = {
+//   eats: true
+// };
+
+// let rabbit = new Rabbit();
+
+// delete Rabbit.prototype.eats;
+
+// console.log( rabbit.eats );
+
+function Obj( userName, userAge, userPost, date, time ) {
+  this.userName = userName;
+  this.userAge = userAge;
+  this.userPost = userPost;
+  this.date = date;
+  this.time = time;
+  console.log( userName + ' ' + "my age: " + userAge + ' my message: ' + userPost + ' ' + date + ' ' + time );
+}
+
+let obj = new Obj("Vlad", 33, 'my work is it important', '20.08.2021', '10:09:36');
+
+let obj2 = Object.assign({}, obj);
+
+obj2.userName = 'Olga';
+// let obj2 = new obj.constructor();
+console.log(obj);
+
+let re = document.querySelector('#re');
+
+for (let key in obj2) {
+  re.innerHTML += '<p>' + obj2[key] + '</p>';
+}
