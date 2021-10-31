790083db3a7b3ea7a96f772ef1d8ff62954c5bdb
VasylenkoVladyslav <vladyslavvasylenko03@gmail.com>
2021-10-31 18:35:22 +0200

    setTimeout change image

--- JS/crismas.js
+++ JS/crismas.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const img = document.querySelector('.img');
+const cont = document.querySelector('.container');
+
+function secondImg() {
+  img.src = "./images/NG/CHG.gif";
+}
+
+function firstImg() {
+  img.src = "./images/NG/SNGB.gif";
+}
+
+setTimeout(secondImg, 10000);
+setTimeout(firstImg, 20000);
