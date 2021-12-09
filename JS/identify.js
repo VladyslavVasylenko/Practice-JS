bd9ec4c4f8ddea6ad7c14b5af1ac6b5e8a9938e4
VasylenkoVladyslav <vladyslavvasylenko03@gmail.com>
2021-12-09 07:16:18 +0200

    snow

--- JS/identify.js
+++ JS/identify.js
@@ -1,44 +1,61 @@
 'use strict';
 
-// const firstBlock = document.getElementById("first");
-const secondBlock = document.getElementById("second");
+const firstBlock = document.getElementById("first");
 const buttonShow = document.querySelector(".form-btn");
 const identify = document.querySelector(".identify");
 const form = document.querySelector(".form");
-let result = [];
-
+let arr = [];
 
 buttonShow.addEventListener('click', function(event) {
   event.preventDefault();
-  const firstBlock = document.getElementById("first").value;
-  // identify.innerHTML = `<p>${result}</p>`;
-  result.push(firstBlock);
-  result.sort();
-  console.log(result);
+
+  if(firstBlock.value == '') {
+    identify.innerHTML = `<p>Write something!</p>`;
+  } else {
+    arr.push(firstBlock.value);
+    // result.push(firstBlock.value);
+    // result.sort();
+    console.log(arr);
+    let result = arr.filter( (item, index) => {
+      return arr.indexOf(item) !== index;
+    });
+    identify.innerHTML = `<p>${result}</p>`;
+    console.log(result);
+  }
 
   form.reset();
 });
 
+// how meny identify
+// for (let len = result.length, i = len; --i >= 0;) {
+//   if (result[result[i]]) {
+//     result[result[i]] += 1;
+//     result.splice(i, 1);
+//   } else {
+//     result[result[i]] = 1;
+//   }
+// }
+
 // Деструктурирующее присваивание
-let salaries = {
-  "John": 1000,
-  "Pete": 3000,
-  "Mary": 550
-}
+// let salaries = {
+//   "John": 1000,
+//   "Pete": 3000,
+//   "Mary": 550
+// }
 
-function topSalary(salaries) {
-  let max = 0;
-  let maxName = null;
+// function topSalary(salaries) {
+//   let max = 0;
+//   let maxName = null;
 
-  for(const [name, salary] of Object.entries(salaries)) {
-    if(max < salary) {
-      max = salary;
-      maxName = name;
-    }
-  }
+//   for(const [name, salary] of Object.entries(salaries)) {
+//     if(max < salary) {
+//       max = salary;
+//       maxName = name;
+//     }
+//   }
 
-  console.log(maxName);
-  return maxName;
-}
+//   console.log(maxName);
+//   return maxName;
+// }
 
-topSalary(salaries);
+// topSalary(salaries);
