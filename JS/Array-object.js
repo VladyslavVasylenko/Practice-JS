d6820a2511fad8013abb109ea515ee2ffce4a37a
Vladyslav <vladyslavvasylenko03@gmail.com>
2022-02-20 18:44:58 +0200

    function get name and surname and must show what he get

--- JS/Array-object.js
+++ JS/Array-object.js
@@ -1,16 +1,29 @@
 'use strict';
 
-const words = ['read', 'old', 'lover', 'fructs', 'me', 'i'];
+// const words = ['read', 'old', 'lover', 'fructs', 'me', 'i'];
 
-let wordsMap = words.map((word) => {
-  return {
-    key: word,
-    charCount: word.length,
-  };
-});
+// let wordsMap = words.map((word) => {
+//   return {
+//     key: word,
+//     charCount: word.length,
+//   };
+// });
 
-console.log(wordsMap);
+// console.log(wordsMap);
 
-let result = words.filter(word => word.length < 4);
+// let result = words.filter(word => word.length < 4);
 
-console.log(result);
+// console.log(result);
+
+const userName = 'Vlad';
+const userSurname = 'Vasylenko';
+
+function person(name, sureName) {
+  if(name !== '' || sureName !== '') {
+    console.log(name + " " + sureName);
+  } else{
+    console.log(' ');
+  }
+}
+
+person(userName, userSurname);
