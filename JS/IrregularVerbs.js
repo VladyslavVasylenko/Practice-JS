2e1f740f064f4410d3bb67a332db3818fe3c2d9a
Vladyslav <vladyslavvasylenko03@gmail.com>
2022-03-03 16:01:10 +0200

    add error and new irregular verb beat

--- JS/IrregularVerbs.js
+++ JS/IrregularVerbs.js
@@ -6,9 +6,13 @@ const answer = document.querySelector('.answer');
 button.addEventListener('click', function(e) {
   e.preventDefault();
   if (input.value == 'buy' || input.value == 'Buy') {
-    answer.innerHTML = '<p>Buy, Bought - (second form PAST SIMPLE), Bought - (three form PAST SIMPLE) - покупать.</p>';
+    answer.innerHTML = '<p>Buy <br> Bought - (second form PAST SIMPLE) <br> Bought - (three form PAST SIMPLE) <br> перевод - покупать.</p>';
+  } else if (input.value == 'beat' || input.value == 'Beat') {
+    answer.innerHTML = '<p>Beat <br> Beat - (second form PAST SIMPLE) <br> Beaten - (three form PAST SIMPLE) <br> перевод - Бить.</p>';
+  } else if (input.value == '') {
+    answer.innerHTML = '<p>You didn`t write word.</p>';
   } else {
-    answer.innerHTML = '<p>It`s not irregular verbs</p>';
+    answer.innerHTML = `<p>${input.value} - it's not irregular verbs!</p>`;
   }
   input.value = '';
 });
