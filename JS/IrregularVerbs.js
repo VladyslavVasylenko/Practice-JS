db57ecebbb75f7123046de75286ed9b30ebb08a1
Vladyslav <vladyslavvasylenko03@gmail.com>
2022-03-12 15:08:51 +0200

    app iregverb and calendar

--- JS/IrregularVerbs.js
+++ JS/IrregularVerbs.js
@@ -18,3 +18,51 @@ button.addEventListener('click', function(e) {
 
   input.value = '';
 });
+
+// let url = 'ru-ua-com';
+
+// function camelize(str) {
+//   return str
+//     .split('-')
+//     .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+//     .join('');
+// }
+
+// console.log(camelize(url));
+
+// function filterRange(ar, a, b) {
+//   return ar.filter(item => (a <= item && item <= b));
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// arr.sort();
+// arr.reverse();
+
+// let filtered = filterRange(arr, 1, 4);
+
+// console.log(filtered);
+// console.log(arr);
+
+const calendar = new Date();
+let d = calendar.toLocaleDateString();
+let c = calendar.toTimeString();
+
+function time() {
+  const calendar = new Date();
+  let h = calendar.getHours();
+  let m = calendar.getMinutes();
+  let s = calendar.getSeconds();
+
+  if (s < 10 || s == 0) s = "0" + s;
+
+  if (m < 10 || m == 0) m = "0" +  m;
+
+  if (h < 10 || h == 0) h = "0" +  h;
+  
+  document.querySelector('.time').innerHTML = ('<p class="time">' + d + '<br>' + h + ' : ' + m + ' : ' + s + '</p>');
+
+  setInterval(time, 1000);
+};
+
+time();
